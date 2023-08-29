cb12b4bf54a2e461ffefe22f28892cdbefe6d3b3
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-08-29 09:21:56 -0300

    bootcamp fase2

---
+++ src/ar/com/educacionit/universidad/HolaMundo.java
@@ -0,0 +1,36 @@
+package ar.com.educacionit.universidad;
+
+public class HolaMundo {
+	
+	//es mi punto de entrada = main
+	public static void main(String[] args) {
+		/*variables
+			tipo nombre;
+			tipo nombre = valor;
+		*/
+		/*numeros enteros (no tiene decimal)*/		
+		byte edad = 127; /*-128 - 127*/
+		short edadShort = 128;		
+		int edadInt;
+		long edadLong;
+		/*numeros reales|flotanteS|decimales*/
+		float edadFloat;
+		double edadDouble;		
+		/*rango de representacion */
+		
+		// byte < short < int < long < float < double
+		
+		//1 caracter
+		char letra = 'A';
+		boolean flag = false;
+		
+		//String
+		String nombre = "carlos";
+		
+		var edadVar = 37;
+		
+		//funciones|arrays|OOP|collections|enums|interfaces|generics|...
+		//crud db
+	}
+
+}
